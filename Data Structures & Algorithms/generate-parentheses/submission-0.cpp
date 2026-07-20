678e3ef9eb581196e8085906956d401e03c90846
Aadish Jain <jain.aadishj@gmail.com>
2026-07-20 12:27:26 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,44 @@
+class Solution {
+   public:
+    vector<char> temp;
+    vector<string> ans;
+    int num;
+    vector<string> generateParenthesis(int n) {
+        num = 2 * n;
+        sol(0);
+        return ans;
+    }
+    bool check(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.empty()) return false;
+
+                if ((c == ')' && st.top() != '(') || (c == '}' && st.top() != '{') ||
+                    (c == ']' && st.top() != '['))
+                    return false;
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+    void sol(int i) {
+        string s(temp.begin(), temp.end());
+        if (i == num) {
+            if(check(s))
+            ans.push_back(s);
+            return;
+        }
+        temp.push_back('(');
+        sol(i + 1);
+        temp.pop_back();
+        temp.push_back(')');
+        sol(i + 1);
+        temp.pop_back();
+    }
+};
